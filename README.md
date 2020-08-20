dd4e5288956c65b9425509e8ae2371e15c67f13a
André Gonçalves <andre.goncalves@outsystems.com>
2020-08-20 16:21:58 +0100

    Created a hook to change the display name in the info.plist file

---
+++ README.md
@@ -0,0 +1,2 @@
+# cordova-plugin-plugin-change-displayname
+Plugin created by OutSystems Experts team to solve an issue related with the name of an application, that started with a +. 
---
+++ scripts/update-ios-app-name.js
@@ -0,0 +1,65 @@
+var fs    = require('fs');     // nodejs.org/api/fs.html
+var plist = require('plist');  // www.npmjs.com/package/plist
+var path = require("path");
+var semver = require('semver');
+
+module.exports = function (context) {
+
+    if(context.opts.platforms.indexOf('ios') === -1) return;
+
+    console.log('Attempting to set app name for iOS');
+
+    var PROJECTROOT = context.opts.projectRoot;
+    var PLATFORMPATH = path.resolve(path.join(PROJECTROOT, 'platforms', 'ios'));
+    
+    var EXTENSION = '.xcodeproj';
+    var targetFiles;    
+    fs.readdir(PLATFORMPATH, function(err, files){
+        targetFiles = files.filter(function(file) {
+            return path.extname(file).toLowerCase() === EXTENSION;
+        });
+        
+        if (!targetFiles){
+            console.log("No " + EXTENSION + " file found. Exiting"); 
+            return;
+        }
+        if (targetFiles.length != 1){
+            console.log("More than one " + EXTENSION + " found. Exiting"); 
+            return;
+        }
+    
+        var PROJECTNAME = path.basename(targetFiles[0], EXTENSION);
+        
+        var INFOPLISTPATH = path.join(PLATFORMPATH, PROJECTNAME, PROJECTNAME + '-Info.plist');
+
+        var xml = fs.readFileSync(INFOPLISTPATH, 'utf8');
+        var obj = plist.parse(xml);
+
+        /**
+         * FIRST APPROACH
+         * Set the CFBundleDisplayName only, using the variable set via the plugin instalation
+         */
+        var name = getConfigParser(context, path.join(PLATFORMPATH, PROJECTNAME, 'config.xml')).getPreference('AppName');
+        obj.CFBundleDisplayName = name;
+        
+         /**
+         * SECOND APPROACH
+         * Clean the CFBundleExecutable so it does not have any invalid chars
+         */
+        //obj.CFBundleExecutable = obj.CFBundleExecutable.replace(/[/\\?%*:|"<>\+]/g, '');
+    
+        xml = plist.build(obj);
+        fs.writeFileSync(INFOPLISTPATH, xml, { encoding: 'utf8' });
+    });
+};
+
+function getConfigParser(context, config) {
+
+    if (semver.lt(context.opts.cordova.version, '5.4.0')) {
+        ConfigParser = context.requireCordovaModule('cordova-lib/src/ConfigParser/ConfigParser');
+    } else {
+        ConfigParser = context.requireCordovaModule('cordova-common/src/ConfigParser/ConfigParser');
+    }
+
+    return new ConfigParser(config);
+}
