bb2fdd391116920f6a2c27a66c906a6d2bb16cf4
André Gonçalves <andre.goncalves@outsystems.com>
2021-04-29 20:45:14 +0100

    Enhanced support for MABS 6.3 and 7

---
+++ scripts/npmInstall.js
@@ -0,0 +1,22 @@
+
+var child_process = require('child_process');
+var {isCordovaAbove} = require("./utils");
+
+module.exports = function (context) {
+    var cordovaAbove8 = isCordovaAbove(context, 8);
+    if (!cordovaAbove8) {
+      var deferral = context.requireCordovaModule("q").defer();
+      child_process.exec('npm install', {cwd:__dirname},
+        function (error) {
+          if (error !== null) {
+            console.log('exec error: ' + error);
+            deferral.reject('npm installation failed');
+          }
+          deferral.resolve();
+      });
+
+      return deferral.promise;
+    }
+    return;
+
+	
