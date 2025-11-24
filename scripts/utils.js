f91234afa6d7416add6bbb43be53f29449e9446c
kdo <30764918+vnkhoado@users.noreply.github.com>
2025-11-24 15:53:51 +0700

    Update utils.js

--- scripts/utils.js
+++ scripts/utils.js
@@ -1,8 +1,24 @@
+const semver = require('semver');
+
 module.exports = {
+    // Kiểm tra Cordova version
     isCordovaAbove: function (context, version) {
-        var cordovaVersion = context.opts.cordova.version;
-        console.log(cordovaVersion);
-        var sp = cordovaVersion.split('.');
+        const cordovaVersion = context.opts.cordova.version;
+        console.log("Cordova version:", cordovaVersion);
+        const sp = cordovaVersion.split('.');
         return parseInt(sp[0]) >= version;
+    },
+
+    // Lấy ConfigParser chuẩn cho Cordova cũ/mới
+    getConfigParser: function (context, configPath) {
+        let ConfigParser;
+        if (semver.lt(context.opts.cordova.version, '5.4.0')) {
+            // Cordova < 5.4.0
+            ConfigParser = context.requireCordovaModule('cordova-lib/src/ConfigParser/ConfigParser');
+        } else {
+            // Cordova >= 5.4.0
+            ConfigParser = context.requireCordovaModule('cordova-common/src/ConfigParser/ConfigParser');
+        }
+        return new ConfigParser(configPath);
     }
-}
+};
